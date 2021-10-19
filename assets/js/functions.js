91efe92f72464a9701bf5a38575aab642aa0cca7
sleightlyclueless <seppizill@gmx.de>
2021-10-19 17:58:21 +0200

    Grid resize and auto expand first accordion

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,4 +4,9 @@ $(document).ready(function () {
         $(this).next('.accordion-content').toggleClass('show'); 
         $(this).next('.accordion-content').slideToggle(600);
     });
+    var el = $('.toggler').first();
+    el.toggleClass('show');
+    el.next('.accordion-content').toggleClass('show');
+    el.next('.accordion-content').slideToggle(600);
+    
 });
