5221b11efa580e0f1c925d917fac9a5e96de9918
sleightlyclueless <seppizill@gmx.de>
2021-10-19 12:27:31 +0200

    Begin Grid Template and Layout animals
    Wenn du weiter machen willst Timo lol

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,1 +1,7 @@
-console.log("Timo stinkt");
+$('.toggler').click(function(){
+    var el = $(this).next('.accordion-content');
+    if (el) {
+        el.toggleClass('inactive');
+        el.toggleClass('active');
+    }
+});
