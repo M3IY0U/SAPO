f76b0efa55e3bbf0f00e3495af6a3089f6314118
sleightlyclueless <seppizill@gmx.de>
2021-10-19 19:43:39 +0200

    Add lightbox

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
+    // Accordion expand and close
     $('.toggler').click(function () {
         $(this).toggleClass('show');
         $(this).next('.accordion-content').toggleClass('show'); 
         $(this).next('.accordion-content').slideToggle(600);
     });
+    // Expand first accordion by default
     var el = $('.toggler').first();
     el.toggleClass('show');
     el.next('.accordion-content').toggleClass('show');
     el.next('.accordion-content').slideToggle(600);
+
+
+    // Lightbox behaviour on animal click
+    $('img').click(function() {
+        var content = '<div class="animalcontainer">' + $(this).closest('.animalcontainer').html() + '</div>';
+        $('.lightbox').html(content);
+        $('.lightbox').addClass('show');
+    })
     
 });
